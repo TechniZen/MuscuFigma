5b1354f46ba11ebe2efff3fc66868683cb927866
TechniZen <technizen82973@gmail.com>
2025-06-12 00:05:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('modeToggle');
+  toggle?.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+  });
+});
